f91ffcd37ce22beb5d75810b3678145f07eb6a31
icy-mirror <sky-blue.0829@hotmail.co.jp>
2018-09-10 20:42:19 +0900

    for/switchの学習

--- startup/helloWorld.go
+++ startup/helloWorld.go
@@ -1,31 +1,28 @@
 package main
 
 import (
-  "os"
-  "flag"
+	"fmt"
+	"strconv"
 )
 
-var omitNewline = flag.Bool("n", false, "don't print final newline.")
-
-const (
-  Space = " "
-  Newline = "\n"
-)
-
-func main()  {
-  flag.Parse()
-  var s string = ""
-  for i := 0; i < flag.NArg(); i++ {
-    if i > 0 {
-      s += Space
-    }
-    s += flag.Arg(i)
-  }
-  if !*omitNewline {
-    s += Newline
-  }
-  os.Stdout.WriteString(s)
-  // comment
-  /*コメントしてみた
-  */
+func main() {
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println(strconv.Itoa(i) + " BuzzFizz")
+		} else if i%3 == 0 {
+			fmt.Println(strconv.Itoa(i) + " Fizz")
+		} else if i%5 == 0 {
+			fmt.Println(strconv.Itoa(i) + " Buzz")
+		}
+	}
+	for s := 1; s <= 100; s++ {
+		switch {
+		case s%15 == 0:
+			fmt.Println(strconv.Itoa(s) + " FUZZBIZZ")
+		case s%3 == 0:
+			fmt.Println(strconv.Itoa(s) + " FUZZ")
+		case s%5 == 0:
+			fmt.Println(strconv.Itoa(s) + " BIZZ")
+		}
+	}
 }
